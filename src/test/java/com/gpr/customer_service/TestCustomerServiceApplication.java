67af2a51c4c23f3bbd28b57919a9ce2d05f01dc2
grai1982 <grai@luxoft.com>
2025-04-05 20:42:30 +0530

    first commit

---
+++ src/test/java/com/gpr/customer_service/TestCustomerServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gpr.customer_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCustomerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CustomerServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
